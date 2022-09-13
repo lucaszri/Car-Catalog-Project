c135e8ab0b14f343cc2a193f62806826701cdf23
Lucas de Souza <lucaszsep@gmail.com>
2022-09-13 18:41:38 -0300

    :sparkles: feat: Criada models Car e Cars.

---
+++ src/models/Car.ts
@@ -0,0 +1,9 @@
+interface Car {
+    id: number
+    title: string;
+    brand: string;
+    price: string;
+    age: number;
+}
+
+export default Car;
---
+++ src/models/Cars.ts
@@ -0,0 +1,9 @@
+interface Cars {
+    id: number
+    title: string;
+    brand: string;
+    price: string;
+    age: number;
+}
+
+export default Cars;
