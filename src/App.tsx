c2fce84fc5bf558ad51af508cdf4cb57552923c0
Lucas de Souza <lucaszsep@gmail.com>
2022-09-17 18:30:17 -0300

    :lipstick: feat: Home :  Estilizaçao CSS do formulario

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/estaticos/navbar/Navbar';
+import Home from './paginas/home/Home';
+import Footer from './components/estaticos/footer/Footer';
 
 function App() {
   return (
     <>
     <Navbar />
+    <Home />
+    <Footer />
     </>
   );
 }
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,8 +1,18 @@
+import { Box, Button } from '@material-ui/core'
 import React from 'react'
+import "./Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <Box className="container-txtbtn">
+      <Box className="txt">
+        <h2> Find and add cars in the world's best catalog</h2>
+      </Box>
+      <Box className="btn">
+        <Button className='btn-home'> <span> find... </span>  </Button>
+        <Button className='btn-home'> <span> add...  </span> </Button>
+      </Box>
+    </Box>
   )
 }
 
